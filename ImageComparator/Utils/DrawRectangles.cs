78562e3105764ecba3da691ae3c83cb743740c67
yuriy.kormin <yuriy.kormin@gmail.com>
2024-12-15 14:59:19 +0400

    use graphics.DrawRectangle to draw in result

--- ImageComparator/Utils/DrawRectangles.cs
+++ ImageComparator/Utils/DrawRectangles.cs
@@ -1,72 +1,25 @@
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
 namespace ImageComparator.Utils
 {
     public class DrawRectangles
     {
-        private static unsafe void SetColor(byte* resPtr, int address, List<int> color)
-        {
-            // Ensure the color list has exactly 3 elements for RGB
-            if (color == null || color.Count != 3)
-            {
-                throw new ArgumentException("Color must have exactly 3 elements (R, G, B).");
-            }
 
-            resPtr[address] = (byte)color[2];    // Red
-            resPtr[address + 1] = (byte)color[1]; // Green
-            resPtr[address + 2] = (byte)color[0]; // Blue
-        }
-
-        public static unsafe void DrawBorder(
-            byte* resPtr, 
-            int x, 
-            int y, 
-            int width, 
-            int height, 
-            int stride, 
-            int bytesPerPixel, 
+        public static void DrawBorder(
+            Bitmap bitmap,
+            int x, int y, int dx, int dy,
             List<int>? color = null)
         {
             // Default color is red if not provided
             color ??= new List<int> { 255, 0, 0 };
-
-            // Draw Top and Bottom borders
-            DrawHorizontalBorderLine(resPtr, x, y, width, bytesPerPixel, stride, color);  // Top border
-            DrawHorizontalBorderLine(resPtr, x, y + height - 1, width, bytesPerPixel, stride, color); // Bottom border
-
-            // Draw Left and Right borders
-            DrawVerticalBorderLine(resPtr, x, y, height, bytesPerPixel, stride, color);  // Left border
-            DrawVerticalBorderLine(resPtr, x + width - 1, y, height, bytesPerPixel, stride, color); // Right border
-        }
-
-        private static unsafe void DrawHorizontalBorderLine(
-            byte* resPtr, 
-            int x, 
-            int y, 
-            int width, 
-            int bytesPerPixel, 
-            int stride, 
-            List<int> color) 
-        {
-            for (int i = 0; i < width; i++)
+            
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            using (Pen pen = new Pen(Color.FromArgb(color[0],color[1],color[2]), 1))
             {
-                int offset = (y * stride) + ((x + i) * bytesPerPixel);
-                SetColor(resPtr, offset, color);
+                graphics.DrawRectangle(pen, new Rectangle(x, y, dx, dy)); 
             }
-        }
-
-        private static unsafe void DrawVerticalBorderLine(
-            byte* resPtr, 
-            int x, 
-            int y, 
-            int height, 
-            int bytesPerPixel, 
-            int stride, 
-            List<int> color) 
-        {
-            for (int j = 0; j < height; j++)
-            {
-                int offset = ((y + j) * stride) + (x * bytesPerPixel);
-                SetColor(resPtr, offset, color);
-            }
+            
         }
     }
 }
