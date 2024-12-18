a432284b04c654135f92c57abbb0f0a58843fb91
yuriy.kormin <yuriy.kormin@gmail.com>
2024-12-18 12:09:33 +0400

    1

--- ImageComparator.Tests/CommonTestLibs/BitmapFixture.cs
+++ ImageComparator.Tests/CommonTestLibs/BitmapFixture.cs
@@ -1,16 +1,19 @@
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Security.Cryptography;
 using ImageComparator.Utils;
 
 namespace ImageComparator.Tests
 {
+
     [TestFixture]
     public class BitmapFixture
     {
         public static Bitmap CreateSolidColorBitmap(int width, int height, Color color)
         {
             var bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly,
+                PixelFormat.Format24bppRgb);
 
             try
             {
@@ -34,11 +37,14 @@ namespace ImageComparator.Tests
             {
                 bitmap.UnlockBits(bitmapData);
             }
+
             return bitmap;
         }
+
         public static unsafe Color GetPixelColor(Bitmap bitmap, int x, int y)
         {
-            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
 
             try
             {
@@ -46,7 +52,7 @@ namespace ImageComparator.Tests
                 byte b = ptr[0];
                 byte g = ptr[1];
                 byte r = ptr[2];
-                return Color.FromArgb(r,g,b);
+                return Color.FromArgb(r, g, b);
             }
             finally
             {
@@ -63,7 +69,28 @@ namespace ImageComparator.Tests
                 g.FillRectangle(brush, x, y, width, height);
             }
         }
+
+        public static string GetBitmapHash(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] bitmapBytes = ms.ToArray();
+
+                // Generate the MD5 hash
+                using (MD5 md5 = MD5.Create())
+                {
+                    byte[] hashBytes = md5.ComputeHash(bitmapBytes);
+                    return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                }
+            }
+        }
+
+        public static bool IsEqualByHash(Bitmap bitmap1, Bitmap bitmap2)
+        {
+            return (GetBitmapHash(bitmap1) == GetBitmapHash(bitmap2));
+        }
     }
-    
+
 };
 
